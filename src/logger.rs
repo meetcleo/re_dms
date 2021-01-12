412b4817ca9af7436605a50d2ad23d7a40f35428
Michael Hauser-Raspe <michael.hauser-raspe@cantab.net>
2021-01-12 17:38:16 +0000

    Refactor.

--- src/logger.rs
+++ src/logger.rs
@@ -3,50 +3,28 @@ use log::{debug, error, info};
 
 pub struct Logger {}
 
-// could deduplicate with a macro, but not gonna bother f'now
 // lets structure our logger so that we can grep our logs more easily
 impl Logger {
     pub fn info(wal_number: Option<u64>, table_name: Option<TableName>, tag: &str, message: &str) {
         info!(
-            "wal:{} table:{} tag:{} message:{}",
-            wal_number
-                .map(|x| format!("{:0>16X}", x))
-                .unwrap_or("none".to_owned()),
-            table_name
-                .map(|x| x.to_string())
-                .unwrap_or("none".to_owned()),
-            tag,
-            message
+            "{}",
+            Self::structured_format(wal_number, table_name, tag, message)
         );
     }
 
     #[allow(dead_code)]
     pub fn error(wal_number: Option<u64>, table_name: Option<TableName>, tag: &str, message: &str) {
         error!(
-            "wal:{} table:{} tag:{} message:{}",
-            wal_number
-                .map(|x| format!("{:0>16X}", x))
-                .unwrap_or("none".to_owned()),
-            table_name
-                .map(|x| x.to_string())
-                .unwrap_or("none".to_owned()),
-            tag,
-            message
+            "{}",
+            Self::structured_format(wal_number, table_name, tag, message)
         );
     }
 
     #[allow(dead_code)]
     pub fn debug(wal_number: Option<u64>, table_name: Option<TableName>, tag: &str, message: &str) {
         debug!(
-            "wal:{} table:{} tag:{} message:{}",
-            wal_number
-                .map(|x| format!("{:0>16X}", x))
-                .unwrap_or("none".to_owned()),
-            table_name
-                .map(|x| x.to_string())
-                .unwrap_or("none".to_owned()),
-            tag,
-            message
+            "{}",
+            Self::structured_format(wal_number, table_name, tag, message)
         );
     }
 
@@ -57,7 +35,18 @@ impl Logger {
         tag: &str,
         message: &str,
     ) {
-        panic!(
+        panic!(Self::structured_format(
+            wal_number, table_name, tag, message
+        ));
+    }
+
+    pub fn structured_format(
+        wal_number: Option<u64>,
+        table_name: Option<TableName>,
+        tag: &str,
+        message: &str,
+    ) -> String {
+        format!(
             "wal:{} table:{} tag:{} message:{}",
             wal_number
                 .map(|x| format!("{:0>16X}", x))
@@ -67,6 +56,6 @@ impl Logger {
                 .unwrap_or("none".to_owned()),
             tag,
             message
-        );
+        )
     }
 }
