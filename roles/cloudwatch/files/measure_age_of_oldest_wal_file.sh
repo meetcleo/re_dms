30f751526bf22d2f9e96af5a36c2e35d6a925802
Michael Hauser-Raspe <michael.hauser-raspe@cantab.net>
2021-11-04 16:22:34 +0000

    Logging of metrics.

--- roles/cloudwatch/files/measure_age_of_oldest_wal_file.sh
+++ roles/cloudwatch/files/measure_age_of_oldest_wal_file.sh
@@ -2,12 +2,16 @@
 set -euo pipefail
 
 # date at which the oldest wal file was last modified in epoch seconds
+echo "calculating age of oldest wal file"
 then_old=$(ls /re_dms/output_wal/*.wal -tr | head -n 1 | xargs stat -c '%Y')
 
 # now epoch seconds
 now=$(date +%s)
 
 # oldest file created seconds ago
-old_seconds_ago=$(($then_old-$now))
+old_seconds_ago=$(($now-$then_old))
 
-echo "seconds_since_oldest_wal_file_modified:$old_seconds_ago|g" | nc -u -w0 127.0.0.1 8125
+data="seconds_since_oldest_wal_file_modified:$old_seconds_ago|g"
+echo $data
+echo $data | nc -u -w1 127.0.0.1 8125
+echo "done calculating age of oldest wal file"
