6736e4d93a5592693486664e762b57a282fa8beb
Michael Hauser-Raspe <michael.hauser-raspe@cantab.net>
2021-11-04 16:22:34 +0000

    Logging of metrics.

--- roles/cloudwatch/files/measure_number_of_re_dms_processes.sh
+++ roles/cloudwatch/files/measure_number_of_re_dms_processes.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "calculating number_of_re_dms_processes_running"
 number_of_re_dms_processes_running=$(ps -C re_dms --no-headers | wc -l)
-echo "number_of_re_dms_processes_running:$number_of_re_dms_processes_running|g" | nc -u -w0 127.0.0.1 8125
+
+data="number_of_re_dms_processes_running:$number_of_re_dms_processes_running|g"
+echo $data
+echo $data | nc -u -w1 127.0.0.1 8125
+echo "done calculating number_of_re_dms_processes_running"
--- roles/cloudwatch/files/measure_number_of_wal_files.sh
+++ roles/cloudwatch/files/measure_number_of_wal_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "calculating number_of_wal_files"
 number_of_wal_files=$(ls /re_dms/output_wal/*.wal | wc -l)
-echo "number_of_wal_files:$number_of_wal_files|g" | nc -u -w0 127.0.0.1 8125
+
+data="number_of_wal_files:$number_of_wal_files|g"
+echo $data
+echo $data | nc -u -w1 127.0.0.1 8125
+echo "done calculating number_of_wal_files"
+
