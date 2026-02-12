e975f7898c208c440d83ea6b0d962fd0a3e6931f
fbad <fabio@meetcleo.com>
2026-02-12 15:56:43 +0100

    fix script for measure age of youngest wal file of tx db

--- roles/cloudwatch/files/tx_measure_age_of_youngest_wal_file.sh
+++ roles/cloudwatch/files/tx_measure_age_of_youngest_wal_file.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # date at which the most recent wal file was last modified in epoch seconds
 echo "calculating age of youngest wal file"
-then_new=$(ls /re_dms/output_wal/*.wal -t | head -n 1 | xargs stat -c '%Y')
+then_new=$(ls /re_dms/transactions_output_wal/*.wal -t | head -n 1 | xargs stat -c '%Y')
 
 # now epoch seconds
 now=$(date +%s)
@@ -11,7 +11,7 @@ now=$(date +%s)
 # oldest file created seconds ago
 newest_seconds_ago=$(($now-$then_new))
 
-data="seconds_since_newest_wal_file_modified:$newest_seconds_ago|g"
+data="tx_seconds_since_newest_wal_file_modified:$newest_seconds_ago|g"
 echo $data
 echo $data | nc -u -w1 127.0.0.1 8125
 echo "done calculating age of youngest wal file"
